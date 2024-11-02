c327b7fd8b433860282783be0d5ea1c67ed2ba52
gpjen <nyonggpj9995@gmail.com>
2024-11-02 23:48:48 +0700

    fix double list

--- LinkedList/typelist/SinggleLinkedlist.go
+++ LinkedList/typelist/SinggleLinkedlist.go
@@ -5,6 +5,25 @@ import (
 	"strings"
 )
 
+type Node struct {
+	Value string
+	Next  *Node
+}
+
+func NewNode(value string) *Node {
+	return &Node{Value: value, Next: nil}
+}
+
+func (n *Node) Append(value string) *Node {
+	n.Next = NewNode(value)
+	return n.Next
+}
+
+func (n *Node) Remove() *Node {
+	n.Next = nil
+	return n.Next
+}
+
 type LinkedList struct {
 	Head   *Node
 	Tail   *Node
