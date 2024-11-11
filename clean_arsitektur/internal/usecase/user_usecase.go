db1df438a2ff864c23258d3920d42f6ba9e47e57
gpjen <nyonggpj9995@gmail.com>
2024-11-12 01:50:58 +0700

    test clean arc

---
+++ clean_arsitektur/internal/usecase/user_usecase.go
@@ -0,0 +1,22 @@
+package usecase
+
+import (
+	"clean_arsitektur/internal/domain/entity"
+	"clean_arsitektur/internal/domain/repository"
+)
+
+type UserUsecase interface {
+	GetUser(id uint) (*entity.User, error)
+}
+
+type userUsecase struct {
+	userRepo repository.UserRepository
+}
+
+func NewUserUsecase(repo repository.UserRepository) UserUsecase {
+	return &userUsecase{userRepo: repo}
+}
+
+func (u *userUsecase) GetUser(id uint) (*entity.User, error) {
+	return u.userRepo.GetUserByID(id)
+}
