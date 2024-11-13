351e84b11e636cbc8ac13c7edc07136b38b2b31c
gpjen <nyonggpj9995@gmail.com>
2024-11-14 00:55:59 +0700

    apply hot reload docker

--- docker/app/main.go
+++ docker/app/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, Dockerized Go with Air! 12345")
+	fmt.Fprintln(w, "Hello, Dockerized Go with Air! 22222")
 }
 
 func main() {
